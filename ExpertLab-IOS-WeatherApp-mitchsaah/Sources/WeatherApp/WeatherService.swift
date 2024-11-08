4339376d5052423399f72ed3c776ea216a9580c7
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2024-11-08 14:33:04 +0100

    Added a function to construct API request URL

--- ExpertLab-IOS-WeatherApp-mitchsaah/Sources/WeatherApp/WeatherService.swift
+++ ExpertLab-IOS-WeatherApp-mitchsaah/Sources/WeatherApp/WeatherService.swift
@@ -4,4 +4,16 @@ import Alamofire
 class WeatherService {
     private let apiKey = "e44c5b1dfd503e307697d439de360179"
     private let baseURL = "https://api.openweathermap.org/data/2.5/weather"
+    
+    // A construction method for the URL
+    func makeRequestURL(for city: String) -> URL? {
+            var components = URLComponents(string: baseURL)
+            components?.queryItems = [
+                URLQueryItem(name: "q", value: city),
+                URLQueryItem(name: "appid", value: apiKey),
+                URLQueryItem(name: "units", value: "metric")
+            ]
+            return components?.url
+        }
 }
+
