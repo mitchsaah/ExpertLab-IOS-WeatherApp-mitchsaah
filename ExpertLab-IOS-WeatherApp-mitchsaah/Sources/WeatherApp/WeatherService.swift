f6f961aac586190beb7c1d80a4cea69614749210
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2024-11-08 14:17:13 +0100

    added API key

---
+++ ExpertLab-IOS-WeatherApp-mitchsaah/Sources/WeatherApp/WeatherService.swift
@@ -0,0 +1,7 @@
+import Foundation
+import Alamofire
+
+class WeatherService {
+    private let apiKey = "e44c5b1dfd503e307697d439de360179"
+    private let baseURL = "https://api.openweathermap.org/data/2.5/weather"
+}
