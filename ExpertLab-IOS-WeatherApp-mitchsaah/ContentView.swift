a5c71ff2326772830c9a6b6046f949eee064395b
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2024-11-08 13:10:31 +0100

    Initial Commit

---
+++ ExpertLab-IOS-WeatherApp-mitchsaah/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpertLab-IOS-WeatherApp-mitchsaah
+//
+//  Created by Mitch Saah on 08/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExpertLab-IOS-WeatherApp-mitchsaah/ExpertLab_IOS_WeatherApp_mitchsaahApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpertLab_IOS_WeatherApp_mitchsaahApp.swift
+//  ExpertLab-IOS-WeatherApp-mitchsaah
+//
+//  Created by Mitch Saah on 08/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpertLab_IOS_WeatherApp_mitchsaahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
