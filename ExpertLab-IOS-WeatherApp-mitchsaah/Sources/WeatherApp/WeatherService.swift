2c56afb4735032693709f3a0e228fddf9f0a608e
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2024-11-08 15:04:16 +0100

    Defined data models for WeatherResponse, Main, and Weather

--- ExpertLab-IOS-WeatherApp-mitchsaah/Sources/WeatherApp/WeatherService.swift
+++ ExpertLab-IOS-WeatherApp-mitchsaah/Sources/WeatherApp/WeatherService.swift
@@ -1,6 +1,21 @@
 import Foundation
 import Alamofire
 
+// Data Models for JSON Response
+struct WeatherResponse: Decodable {
+    let main: Main
+    let weather: [Weather]
+    let name: String
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let description: String
+}
+
 class WeatherService {
     private let apiKey = "e44c5b1dfd503e307697d439de360179"
     private let baseURL = "https://api.openweathermap.org/data/2.5/weather"
