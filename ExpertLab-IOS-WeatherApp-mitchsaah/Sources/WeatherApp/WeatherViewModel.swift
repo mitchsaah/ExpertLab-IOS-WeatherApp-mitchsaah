a3536f3d68c0a544ff2e5192556ff9447e3362c7
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2024-11-09 13:29:10 +0100

    Added a WeatherService dependency

--- ExpertLab-IOS-WeatherApp-mitchsaah/Sources/WeatherApp/WeatherViewModel.swift
+++ ExpertLab-IOS-WeatherApp-mitchsaah/Sources/WeatherApp/WeatherViewModel.swift
@@ -5,4 +5,6 @@ class WeatherViewModel: ObservableObject {
     @Published var cityName: String = "Enter a City"
     @Published var temperature: String = "--°C"
     @Published var description: String = "Weather data will appear here"
+    
+    private let weatherService = WeatherService()
 }
