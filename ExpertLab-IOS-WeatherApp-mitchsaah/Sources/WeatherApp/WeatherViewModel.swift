9237ac57167c3df5f82161f0274250da0cc500b7
mitchsaah <71660816+mitchsaah@users.noreply.github.com>
2024-11-09 13:18:01 +0100

    Observable properties and placeholder values

--- ExpertLab-IOS-WeatherApp-mitchsaah/Sources/WeatherApp/WeatherViewModel.swift
+++ ExpertLab-IOS-WeatherApp-mitchsaah/Sources/WeatherApp/WeatherViewModel.swift
@@ -1,7 +1,8 @@
-//
-//  WeatherViewModel.swift
-//  ExpertLab-IOS-WeatherApp-mitchsaah
-//
-//  Created by Mitch Saah on 09/11/2024.
-//
+import SwiftUI
+import Combine
 
+class WeatherViewModel: ObservableObject {
+    @Published var cityName: String = "Enter a City"
+    @Published var temperature: String = "--°C"
+    @Published var description: String = "Weather data will appear here"
+}
